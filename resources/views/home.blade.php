c028af368ac051304548395cf2da081ba7984d7d
grover641 <2022210232@udh.edu.pe>
2025-06-26 19:09:29 -0500

    Avance Proyecto LP3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Formulario de búsqueda</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,7 +16,46 @@
 
                     {{ __('') }}
 
-                    <h1>Plataforma de Citas Médicas para Zonas Rurales</h1>
+
+
+                    <div class="col-md-12">
+                        <form method="get" action="/lista">
+                            <div class="input-group mb-3">
+                                <select class="form-select" name="especialidad" placeholder="Especialidad" aria-label="Username" required>
+                                    <option value="">Selecciona una especialidad</option>
+                                    <option value="1">Medicina General</option>
+                                    <option value="2">Pediatria</option>
+                                    <option value="3">Traumatología (huesos)</option>
+                                    <option value="4">Obstetricia</option>
+                                    <option value="5">Ginecología</option>
+                                    <option value="6">Psicología</option>                                    
+                                </select>
+                                <span class="input-group-text">-</span>
+                                <select class="form-select" name="distrito" placeholder="Distrito" aria-label="Username" required>
+                                    <option value="">Selecciona un distrito</option>
+                                    <option value="1">Amarilis</option>
+                                    <option value="2">Chinchao</option>
+                                    <option value="3">Churubamba</option>
+                                    <option value="4">Huánuco</option>
+                                    <option value="5">Margos</option>
+                                    <option value="6">Pillco Marca</option>
+                                    <option value="7">Quisqui</option>
+                                    <option value="8">San Francisco de Cayrán</option>
+                                    <option value="9">San Pedro de Chaulán</option>
+                                    <option value="10">Santa María del Valle</option>
+                                    <option value="11">Yarumayo</option>                                  
+                                </select>
+                            </div>
+                            <div class="input-group mt-5 justify-content-md-center">
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <!-- <button class="btn btn-primary text-center" type="submit">Buscar Cita</button> -->
+                                    <a href="/citas/lista" role="button" class="btn btn-primary text-center">Buscar Cita</a>                                
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,26 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//Route::get('/lista')->name('citas.lista');
+
+/*Route::get('/lista', function () {
+    return view('citas.lista');
+});*/
+
+//Route::view('/citas/lista', 'citas.lista')->middleware('auth');
 
 
 Route::middleware(['auth'])->group(function () {
-    
 
+Route::get('/citas/lista', function () {
+    return view('citas.lista');
+});
+    
+/*
     Route::resource('proyectos', ProyectoController::class);
     Route::resource('tareas', TareaController::class);
     Route::resource('comentarios', ComentarioController::class);
 
     Route::get('/proyecto/{proyecto}', [ProyectoController::class, 'show'])->name('proyectos.show');
-
+*/
 });
